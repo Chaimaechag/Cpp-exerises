43dc12a26e1c06b731e831e9b65db3761222f2fa
Chaimae Chag <162185746+Chaimaechag@users.noreply.github.com>
2026-03-11 06:57:15 +0000

    Add multiplication table program in C++

---
+++ Proplem01_Multiplcation_table.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void PrintTableHeader()
+{
+	cout << "\n\n\t\t\tMultiplication Table Frome 1 to 10 \n\n";
+	cout << "\t";
+	for (int i = 1; i <= 10; i++)
+	{
+		cout <<  i << "\t";
+	}
+	cout << "\n----------------------------------------------------------------------------------------------\n";
+}
+
+string ColumSperator(int i)
+{
+	if (i < 10)
+		return "   |";
+	else
+		return "  |";
+}
+void PrintMultiplicationTabl()
+{
+	PrintTableHeader();
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << " " << i << ColumSperator(i) << "\t";
+		for (int j = 1; j <= 10; j++)
+		{
+			cout  << i * j <<"\t";
+		}
+		cout << endl;
+	}
+
+}
+int main()
+{
+	PrintMultiplicationTabl();
+	return 0;
+}
